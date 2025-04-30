fix: improve computation speed by optionally allowing to specify new solution candidates

sampling_planners/planners/base_planner.py
@@ -5,6 +5,7 @@ from collections.abc import Sequence
 import numpy as np
 
 from sampling_planners.envs.base_env import BaseEnv
+from time import perf_counter
 
 
 class BaseTreePlanner(ABC):
@@ -46,8 +47,9 @@ class BaseTreePlanner(ABC):
         self.n_steps += 1
         n_nodes_last = self.n_nodes
 
-        self.step_body()
-        sol_nodes = [node for node in np.arange(self.n_nodes) if self.is_solution(node)]
+        candidates = self.step_body()
+        candidates = candidates if candidates is not None else np.arange(self.n_nodes)
+        sol_nodes = [node for node in candidates if self.is_solution(node)]
         if len(sol_nodes) != 0:
             closest_sol_node = sol_nodes[np.argmin(self.distance_from_start[sol_nodes])]
             path_to_sol = [closest_sol_node]
@@ -71,7 +73,7 @@ class BaseTreePlanner(ABC):
         return sol_found
 
     @abstractmethod
-    def step_body(self):
+    def step_body(self) -> np.ndarray | None:
         pass
 
     def sample_state(self) -> np.ndarray:

sampling_planners/planners/primitive_planner.py
@@ -17,7 +17,7 @@ class PrimitiveTreePlanner(BaseTreePlanner):
         super().__init__(start, goal, env, sol_threshold, extend_step=extend_step)
         self.choose_nearest = choose_nearest
 
-    def step_body(self):
+    def step_body(self) -> np.ndarray | None:
         # Step 2. Selecting a random node from the tree
         init_node = None
         if self.choose_nearest:
@@ -37,7 +37,7 @@ class PrimitiveTreePlanner(BaseTreePlanner):
         match len(extended_state):
             case 0:
                 # No new nodes added
-                return
+                return []
 
             case 1:
                 # One new node added
@@ -70,3 +70,5 @@ class PrimitiveTreePlanner(BaseTreePlanner):
         )
 
         self.add_edges(new_edges)
+
+        return new_nodes

sampling_planners/planners/rrt_planner.py
@@ -17,7 +17,7 @@ class RRTPlanner(BaseTreePlanner):
         super().__init__(start, goal, env, sol_threshold, extend_step=extend_step)
         self.p_sample_goal = p_sample_goal
 
-    def step_body(self) -> bool:
+    def step_body(self) -> np.ndarray | None:
         # Step 2. Select a new random environment collision-free state
         sample_goal = np.random.rand() < self.p_sample_goal
         new_state = self.sample_state() if not sample_goal else self.goal
@@ -32,7 +32,7 @@ class RRTPlanner(BaseTreePlanner):
         match len(extended_state):
             case 0:
                 # No new nodes added
-                return
+                return []
 
             case 1:
                 # One new node added
@@ -65,3 +65,5 @@ class RRTPlanner(BaseTreePlanner):
         )
 
         self.add_edges(new_edges)
+
+        return new_nodes

sampling_planners/planners/rrt_star_planner.py
@@ -25,13 +25,13 @@ class RrtStarPlanner(BaseTreePlanner):
 
         self.iter_count = 0
 
-    def step_body(self) -> bool:
+    def step_body(self) -> np.ndarray | None:
         self.iter_count += 1
         x_rand = self.sample_state() if np.random.rand() > self.p_sample_goal else self.goal
         nodes_near = self.near(x_rand)
         closest_parent_node, sigma_min = self.choose_parent(nodes_near, x_rand)
         if closest_parent_node == -1 or not self.check_collision_free(self.tree_nodes[closest_parent_node], x_rand):
-            return
+            return []
         new_node = self.add_nodes([x_rand]).item()
         self.distance_from_start = np.concatenate([self.distance_from_start, [sigma_min]])
         self.parent_node = np.concatenate([self.parent_node, [closest_parent_node]])